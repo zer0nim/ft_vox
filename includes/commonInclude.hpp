3ec1e9c1f6e308c1b89e423820796c18c5c4118e
tnicolas42 <tnicolas@student.42.fr>
2019-11-19 23:20:26 +0100

    update win size

--- includes/commonInclude.hpp
+++ includes/commonInclude.hpp
@@ -3,8 +3,8 @@
 
 #define DEBUG true  // type: bool -> enable / disable the debug mode
 #define DEBUG_FPS_LOW DEBUG & false
-#define SCREEN_W 800  // [px] type: int ->width of the screen in pixels
-#define SCREEN_H 600  // [px] type: int -> height of the screen in pixels
+#define SCREEN_W 1400  // [px] type: int ->width of the screen in pixels
+#define SCREEN_H 800  // [px] type: int -> height of the screen in pixels
 #define FPS 120  // [Hz] type: int -> fps of the main loop
 #define LOOP_TIME (1000 / FPS)  // [s] type: float -> duration in s for a loop
 
