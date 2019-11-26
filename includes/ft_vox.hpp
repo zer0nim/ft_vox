55023213ba4b5d3cb4bcec7ac9c63cf955265ded
tnicolas42 <tnicolas@student.42.fr>
2019-11-26 12:23:19 +0100

    resolve bug when printing blocks upper than 128

--- includes/ft_vox.hpp
+++ includes/ft_vox.hpp
@@ -40,7 +40,7 @@ filesystem
 */
 #define CHUNK_EXTENSION ".chunk"
 
-typedef glm::tvec3<int8_t>	chunkVec3;  // used for chunk coordinate
+typedef glm::tvec3<uint8_t>	chunkVec3;  // used for chunk coordinate
 typedef glm::tvec3<int32_t>	wordIVec3;  // used for word coordinate in int (bloc)
 typedef glm::tvec3<float>	wordFVec3;  // user for word coordinate in float (camera)
 
