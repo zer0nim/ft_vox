fe0f10588128a4475bc2927cd63f0f9a5a2e7075
zer0nim <emarin@student.42.fr>
2020-01-22 15:03:00 +0100

    edit glm import

--- includes/commonInclude.hpp
+++ includes/commonInclude.hpp
@@ -25,13 +25,14 @@
 #include <mutex>
 
 // glm
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include "glm/gtx/string_cast.hpp"  // to print vect/mat with glm::to_string
 
 namespace glm {
-  template <typename T, precision P>
-  bool operator<(tvec3<T, P> const &lhs, tvec3<T, P> const &rhs) {
+  template <typename T>
+  bool operator<(tvec3<T> const &lhs, tvec3<T> const &rhs) {
     return (lhs.x < rhs.x || (lhs.x == rhs.x && lhs.y < rhs.y)
 	|| (lhs.x < rhs.x && lhs.y < rhs.y)
 	|| (lhs.x == rhs.x && lhs.y == rhs.y && lhs.z < rhs.z));
